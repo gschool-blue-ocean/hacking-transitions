c4c671f12493997ba325681d4a5ad84cf6bee190
Moses <mov7963@gmail.com>
2022-09-29 16:14:56 -0700

    Refactored 404 handling function

--- utility/index.js
+++ utility/index.js
@@ -1,3 +1,10 @@
-export const checkApiMethod = ({method},string) => method === string
-export const notFound404 = (res) => res.send('404 Not Found')
-export const server = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'deployed website url'
+/**********  API Utility Functions **********/
+
+export const checkApiMethod = ({ method }, string) => method === string;
+export const notFound404 = (res) =>
+  res.status(404).setHeader("Content-type", "text/plain").send("404 Not Found");
+/********** Update Base Url For API request **********/
+export const server =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "deployed website url";
