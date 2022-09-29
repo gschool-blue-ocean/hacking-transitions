a95b1f300b48cd46cf9fe1d8e8d259c8edf9abff
Moses <mov7963@gmail.com>
2022-09-29 12:37:12 -0700

    Merged with pauls branch

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import postgres from "postgres";
 const sql = postgres(process.env.DATABASE_URL);
 export default sql;
-=======
-import {Pool} from 'pg'
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
-export default pool
 
 
 
@@ -25,7 +18,3 @@ export default pool
 
 
 
-
-
-
->>>>>>> 6f19e8c (initial commit)
