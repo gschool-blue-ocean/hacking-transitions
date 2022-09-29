114c848384801654218332adcb8e8eac41e2535f
Moses <mov7963@gmail.com>
2022-09-29 12:32:28 -0700

    Renamed Redux Store

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import store from "../redux";
+import store from "../redux/store";
 import { Provider } from "react-redux";
 function MyApp({ Component, pageProps }) {
   return (
