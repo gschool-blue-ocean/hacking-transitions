6f19e8cec0766f5c755402839bdcea70d1a9a6ea
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-29 13:05:41 -0400

    initial commit

---
+++ database/connection.js
@@ -0,0 +1,25 @@
+import {Pool} from 'pg'
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+export default pool
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ database/seed.sql
@@ -0,0 +1,10 @@
+insert into cohorts (cohort_name, start_date, end_date, active) values ('MCSP-00', 'All', 'Archived Students', false)
+,('MCSP-12', '4/5/2022', '8/12/2022', false)
+,('MCSP-16', '8/16/2022', '12/16/2022', true)
+,('MCSP-17', '9/13/2022', '1/13/2023', true)
+,('MCSP-18', '10/11/2022', '2/10/2023', true)
+,('MCSP-19', '11/8/2022', '3/10/2023', true)
+,('MCSP-20', '12/6/2022', '4/7/2023', true)
+,('MCSP-21', '1/10/2023', '5/12/2023', true);
+
+\i usersSeed.sql
