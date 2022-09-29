4489679856ed0b028e8052ea8a1675bd9da4ddfb
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-29 15:35:13 -0400

    small changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 import "../styles/globals.css";
 import store from "../redux";
 import { Provider } from "react-redux";
+import styles from "../styles/GlobalContainer.module.css"
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <div className={styles.className}>
       <Component {...pageProps} />{" "}
+      </div>
     </Provider>
   );
 }
 
 export default MyApp;
-=======
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
->>>>>>> 6f19e8c (initial commit)
