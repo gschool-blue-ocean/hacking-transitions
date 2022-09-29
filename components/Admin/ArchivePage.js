57e920008010b83ae28f546ff97c172def9b1938
Ilsa <hamptonilsa96@gmail.com>
2022-09-29 15:19:27 -0400

    first/trial commit

---
+++ components/Admin/ArchivePage.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function ArchivePage() {
+  return <div>ArchivePage</div>;
+}
+//comment
---
+++ pages/archive.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function archive() {
+  return <div>archive</div>;
+}
