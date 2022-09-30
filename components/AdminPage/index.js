30f63f5ea5e26adb9d54d3fdae2dcfc132e03177
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-30 17:29:36 -0400

    worked on admin page

--- components/AdminPage/index.js
+++ components/AdminPage/index.js
@@ -1,6 +1,15 @@
+import s from '../../styles/AdminPage.module.css'
 const AdminContainer = () => {
   return (
-    <div>AdminContainer</div>
+    <div className={s.container}>
+      <div className={s.menucontainer}> 
+        <div className={s.menutitle}>
+         Cohorts
+        </div>
+        <div className={s.cohortsmenu}> </div>
+      </div>
+      
+    </div>
   )
 }
 
