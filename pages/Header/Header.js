3e4a9ac5b8d3907f663df1e80ae948ea98480cad
Jacob <jacob.d.walters22@gmail.com>
2022-09-30 13:59:02 -0400

    header mostly complete

--- pages/Header/Header.js
+++ pages/Header/Header.js
@@ -1,15 +1,32 @@
 import React from "react";
 import style from "../../styles/Header.module.css";
-import Logo from "./galvanizeLogo.svg";
+// import Logo from "./galvanizeLogo.svg";
 
 const Header = () => {
   return (
-    <header className={style.header}>
-      header
-      <div className={style.imageContainer}>
-        <img src={Logo} alt="galvanizaeLogo" className={style.logo}></img>
+    <nav className={style.header}>
+      <div className={style.topNav}>
+        <ul className={style.topList}>
+          <li className={style.listItem}>UserName</li>
+          <li className={style.listItem}>Logout</li>
+        </ul>
       </div>
-    </header>
+      <div className={style.bottomNav}>
+        <img
+          src="https://www.galvanize.com/images/galvanize-logo.svg"
+          alt="galvanizaeLogo"
+          className={style.logo}
+        ></img>
+        <h1 className={style.title}>Hacking Transitions</h1>
+        <div className={style.pages}>
+          <ul className={style.bottomList}>
+            <li className={style.page}>Home</li>
+            <li className={style.page}>Archive</li>
+            <li className={style.page}>Admin</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
