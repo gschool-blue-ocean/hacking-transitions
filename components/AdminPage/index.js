c41553ab350b34545058e9bf7dff1a3478e7a346
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-30 18:00:33 -0400

    worked on admin page

--- components/AdminPage/index.js
+++ components/AdminPage/index.js
@@ -1,12 +1,31 @@
 import s from '../../styles/AdminPage.module.css'
 const AdminContainer = () => {
+  const cohorts = [
+    'MCSP-11',
+    'MCSP-12',
+    'MCSP-13',
+    'MCSP-14',
+    'MCSP-15'
+  ]
+  const handleClick = () => {
+    console.log('click')
+  }
   return (
     <div className={s.container}>
       <div className={s.menucontainer}> 
         <div className={s.menutitle}>
          Cohorts
         </div>
-        <div className={s.cohortsmenu}> </div>
+        <div className={s.cohortsmenu}> 
+          <ul>
+            {cohorts.map(cohort => {return (
+                <li>
+                  <btn onClick={handleClick}>{cohort}</btn>
+                </li>
+            )}
+            )}
+          </ul>
+        </div>
       </div>
       
     </div>
