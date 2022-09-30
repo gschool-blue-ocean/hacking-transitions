35a70d44acc5516f2ff754e6ebd55fa67b7c4751
Moses <mov7963@gmail.com>
2022-09-30 15:33:43 -0700

    Made header/footer global

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import "../styles/globals.css";
 import store from "../redux/store";
 import { Provider } from "react-redux";
-import Meta  from "../components/meta";
-import styles from "../styles/GlobalContainer.module.css"
+import Meta from "../components/Meta";
+import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Meta />
-      <div className={styles.container}>
-       <Component {...pageProps} />{" "}
-      </div>
+      <Layout>
+        <Component {...pageProps} />{" "}
+      </Layout>
     </Provider>
   );
 }
 
 export default MyApp;
-
