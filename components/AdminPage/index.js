a7d3afb76b5ea064f7661fc458c2b1635479636f
Paul McDonald <pmcdonald1012@gmail.com>
2022-10-01 12:38:27 -0400

    added some animation features to the admin page, and a some html skeleton

--- components/AdminPage/index.js
+++ components/AdminPage/index.js
@@ -1,4 +1,6 @@
 import s from '../../styles/AdminPage.module.css'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 const AdminContainer = () => {
   const cohorts = [
     'MCSP-11',
@@ -10,21 +12,57 @@ const AdminContainer = () => {
   const handleClick = () => {
     console.log('click')
   }
+  const [isClicked, toggleClicked] = useState(false);
+  const toggleClickedMenu = () => {
+    toggleHClicked(!isClicked);
+  };
+  // const [isMouse, toggleMouse] = React.useState(false);
+  // const toggleMouseMenu = () => {
+  //   toggleMouse(!isMouse);
+  // };
+  //animation for 
+  const subMenuAnimate = {
+    enter: {
+      opacity: 1,
+      rotateX: 0,
+      transition: {
+        duration: 0.5
+      },
+      display: "block"
+    },
+    exit: {
+      opacity: 0,
+      rotateX: -15,
+      transition: {
+        duration: 0.5,
+        delay: 0.3
+      },
+      transitionEnd: {
+        display: "none"
+      }
+    }
+  }
   return (
     <div className={s.container}>
       <div className={s.menucontainer}> 
         <div className={s.menutitle}>
+          <motion.btn 
+            onClick={toggleHoverMenu}>
          Cohorts
+         </motion.btn>
         </div>
         <div className={s.cohortsmenu}> 
-          <ul>
+          <motion.ul 
+              initial="exit"
+              animate={isHover ? "enter" : "exit"}
+              variants={subMenuAnimate}>
             {cohorts.map(cohort => {return (
                 <li className={s.listitem}>
                   <btn className={s.cohortbtn} onClick={handleClick}>{cohort}</btn>
                 </li>
+             )}
             )}
-            )}
-          </ul>
+          </motion.ul >
         </div>
       </div>
       
