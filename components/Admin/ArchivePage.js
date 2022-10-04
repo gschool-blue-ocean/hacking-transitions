46a558433f39d14783575cfe403ca8dfa0f66ff9
Ilsa <hamptonilsa96@gmail.com>
2022-10-04 10:49:40 -0400

    had issues with pulling, added archive notes

--- components/Admin/ArchivePage.js
+++ components/Admin/ArchivePage.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+//goal for this component
+//archive students button, puts checkboxes next to student names, can select students then push "OK"--> get message that student was archived
+//archive cohort button, puts checkboxes next to cohorts, can select multiple then push "OK"--> get message that cohort is archived
+//once state of "archived" should no longer display on other screens
+//should have a display archived button
 export default function ArchivePage() {
+  //STATES NEEDED: global=> archived(student/cohort data), local=> checked(boolean), chooseCohorts(boolean), chooseStudents(boolean)
+
   return <div>ArchivePage</div>;
 }
-//comment
