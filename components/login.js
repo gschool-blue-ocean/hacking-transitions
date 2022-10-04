4fd98a8e23128fabce6b2773d28de54709fd341e
Justin Melendez <104957885+CountDown2Extinction@users.noreply.github.com>
2022-10-04 14:35:42 -0500

    please work

--- components/login.js
+++ components/login.js
@@ -4,7 +4,7 @@ import style from "../styles/LoginStyles.module.css";
 import { server } from "../utility";
 import { setAllUserData, setAllCohortData } from "../redux/features/app-slice.js"
 import { useSelector, useDispatch } from "react-redux";
-import style from "../styles/LoginStyles.module.css"
+
 
 
 let Login = () => {
@@ -31,13 +31,13 @@ let Login = () => {
 
     // useEffect(() => {
     //   (async () => {
-    //     const allUsers = await (await fetch(${server}/api/users)).json();
+    //     const allUsers = await (await fetch(`${server}/api/users`)).json();
     //     dispatch(setAllUserData(allUsers));
-
+        
     //   })();
     //  allUsersData.forEach((user) =>{
     //     return listOfUserNames.push(user.username)
-
+       
     //   })
     // }, []);
 
@@ -57,68 +57,68 @@ let Login = () => {
         body: JSON.stringify(inputData.username)
       })
       .then((res) => console.log(res))
-
-
+      
+   
     }
     const handleChange = (e) =>{
-      setLoginData((prevLoginData) =>{
-          return{
-              ...prevLoginData, 
-              [e.target.name]: e.target.value,
-          }
-      })
-  }
-
-  // handleHash was here but was commented out; 
-  return (
-      <div className={style.modalContainer}>
-      {/* <button onClick={handleHash}>CLICK TO HASH</button> /}*/
-
-      <div className={style.loginContainer}>
-        <h1 className={style.loginTitle}>Hacking Transition</h1>
-        <span id="blankLoginErrMsg" className={style.errorMsg}>
-          Fields can not be blank!
-        </span>
-
-        <form className={style.loginForm} onSubmit={handleSubmit}>
-          <input
-            id="formInput"
-            className={`${style.loginInputBox} ${style.username}`}
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={loginData.username}
-            onChange={handleChange}
-          />
-          <span id="usernameLoginErrMsg" className={style.errorMsg}>
-            Username Not Found!
+        setLoginData((prevLoginData) =>{
+            return{
+                ...prevLoginData, 
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+    
+    // handleHash was here but was commented out; 
+    return (
+        <div className={style.modalContainer}>
+        {/* <button onClick={handleHash}>CLICK TO HASH</button> */}
+  
+        <div className={style.loginContainer}>
+          <h1 className={style.loginTitle}>Hacking Transition</h1>
+          <span id="blankLoginErrMsg" className={style.errorMsg}>
+            Fields can not be blank!
           </span>
-
-          <input
-            id="formInput" 
-            className={style.loginInputBox}
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={loginData.password}
-            onChange={handleChange}
-          />
-          <span id="passwordLoginErrMsg" className={style.errorMsg}>
-            Incorrect Password!
-          </span>
-
-          <button type="submit" className={style.loginBtn}>
-            LOG IN <CgEnter />{" "}
-          </button>
-
-          {/* <button
-                        type='button'
-                        className={${style.loginBtn} ${style.createAccBtn}}
-                        onClick={handleShowCreateAccModal}>Create an Account</button> */}
-        </form>
+  
+          <form className={style.loginForm} onSubmit={handleSubmit}>
+            <input
+              id="formInput"  
+              className={`${style.loginInputBox} ${style.username}`}
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={loginData.username}
+              onChange={handleChange}
+            />
+            <span id="usernameLoginErrMsg" className={style.errorMsg}>
+              Username Not Found!
+            </span>
+  
+            <input
+              id="formInput" 
+              className={style.loginInputBox}
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={loginData.password}
+              onChange={handleChange}
+            />
+            <span id="passwordLoginErrMsg" className={style.errorMsg}>
+              Incorrect Password!
+            </span>
+  
+            <button type="submit" className={style.loginBtn}>
+              LOG IN <CgEnter />{" "}
+            </button>
+  
+            {/* <button
+                          type='button'
+                          className={`${style.loginBtn} ${style.createAccBtn}`}
+                          onClick={handleShowCreateAccModal}>Create an Account</button> */}
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 
