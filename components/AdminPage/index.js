11794cb36694c4d1e07c488d7aaa2223ff9ece3c
Paul McDonald <pmcdonald1012@gmail.com>
2022-10-04 17:51:23 -0400

    some weird issues

--- components/AdminPage/index.js
+++ components/AdminPage/index.js
@@ -1,71 +1,31 @@
 import s from '../../styles/AdminPage.module.css'
-import { useState } from 'react'
-import { motion } from 'framer-motion'
+import CohortMenu from './CohortMenu'
+import CohortView from './CohortView'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 const AdminContainer = () => {
-  //FAKE DATA
-  const cohorts = [
-    'MCSP-11',
-    'MCSP-12',
-    'MCSP-13',
-    'MCSP-14',
-    'MCSP-15'
-  ]
-  const handleClick = () => {
-    console.log('click')
-  }
-  const [isClicked, toggleClicked] = useState(false);
-  const toggleClickedMenu = () => {
-    toggleClicked(!isClicked);
-  };
-  //animation for dropdown menu
-  const subMenuAnimate = {
-    enter: {
-      opacity: 1,
-      rotateX: 0,
-      transition: {
-        duration: 0.5
-      },
-      display: "block"
-    },
-    exit: {
-      opacity: 0,
-      rotateX: -15,
-      transition: {
-        duration: 0.5,
-        delay: 0.3
-      },
-      transitionEnd: {
-        display: "none"
-      }
-    }
-  }
+  const [students, setStudents] = useState([])
+  const [cohorts, setCohorts] = useState([])
+  const [currCohort, setCurrCohort] = useState([])
+
+  //let filtStudents = students.filter((student) => student.cohort_id == id)
+  // setCurrCohort({cohort_id: id, cohort_name: data.cohort_name, students: filtStudents})
+  useEffect(() => {
+    axios({
+        method: 'get',
+        url: '/api/users/students',
+    }).then((res) => setStudents(res.data))
+    axios({
+        method: 'get',
+        url: '/api/cohorts'
+    }).then((res) => setCohorts(res.data))
+  }, [])
   return (
     <div className={s.background}>
-    <div className={s.container}>
-      <div className={s.menucontainer}> 
-        <div className={s.menutitle}>
-          <motion.btn 
-            onClick={toggleClickedMenu}
-            className={s.titlebtn}>
-         Cohorts
-         </motion.btn>
-        </div>
-        <div className={s.cohortsmenu}> 
-          <motion.div 
-              initial="exit"
-              animate={isClicked ? "enter" : "exit"}
-              variants={subMenuAnimate}>
-            {cohorts.map(cohort => {return (
-                <div className={s.listitem}>
-                  <btn className={s.cohortbtn} onClick={handleClick}>{cohort}</btn>
-                </div>
-             )}
-            )}
-          </motion.div >
-        </div>
+      <div className={s.container}>
+        <CohortMenu cohorts={cohorts} currCohort={currCohort} setCurrCohort={setCurrCohort} students={students} />
+        <CohortView students={students} currCohort={currCohort} />
       </div>
-      
-    </div>
     </div>
   )
 }
