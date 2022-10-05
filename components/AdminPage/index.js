70e9b74af4e05996debab64098cfc5f9cc3d922c
Paul McDonald <pmcdonald1012@gmail.com>
2022-10-05 14:26:25 -0400

    new features

--- components/AdminPage/index.js
+++ components/AdminPage/index.js
@@ -6,19 +6,18 @@ import axios from 'axios'
 const AdminContainer = () => {
   const [students, setStudents] = useState([])
   const [cohorts, setCohorts] = useState([])
-
   const [currCohort, setCurrCohort] = useState([])
-  //let filtStudents = students.filter((student) => student.cohort_id == id)
-  // setCurrCohort({cohort_id: id, cohort_name: data.cohort_name, students: filtStudents})
   useEffect(() => {
     axios({
-        method: 'get',
-        url: '/api/users/students',
+      method: 'get',
+      url: '/api/users/students',
     }).then((res) => setStudents(res.data))
     axios({
-        method: 'get',
-        url: '/api/cohorts'
+      method: 'get',
+      url: '/api/cohorts'
     }).then((res) => setCohorts(res.data))
+    // let filtStudents = students.filter((student) => student.cohort_id == id)
+    // setCurrCohort({cohort_id: id, cohort_name: data.cohort_name, students: filtStudents})
   }, [])
   return (
     <div className={s.background}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import store from "../redux/store";
 import { Provider } from "react-redux";
-import Meta  from "../components/meta";
+import Layout from "../components/Layout";
+// import Meta  from "../components/meta";
 import styles from "../styles/GlobalContainer.module.css"
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Meta />
+      {/* <Meta /> */}
       <Layout>
         <Component {...pageProps} />{" "}
       </Layout>
