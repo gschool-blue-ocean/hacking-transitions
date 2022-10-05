ea4c23255623d4ff43053e2ae92160601d4a56d8
Moses <mov7963@gmail.com>
2022-10-05 12:34:21 -0700

    Added by user route to user API

--- components/login.js
+++ components/login.js
@@ -38,11 +38,7 @@ let Login = () => {
         username: loginData.username,
         password: loginData.password
       }
-    fetch(`${server}/api/users/userbyname/${inputData.username}`, {
-        method: 'POST', 
-        headers: {"Content-Type" : "application/json"}, 
-        body: JSON.stringify(inputData.username)
-      })
+    fetch(`${server}/api/users/userbyname/${inputData.username}`)
       .then(res => res.json()
       .then((data) => {  
     
