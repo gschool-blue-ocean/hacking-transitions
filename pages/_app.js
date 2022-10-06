c58c3e2c1810b17fabed3efa1ae69faa4cb36fff
Justin Melendez <104957885+CountDown2Extinction@users.noreply.github.com>
2022-10-06 10:20:42 -0500

    still pulling from main

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import store from "../redux/store";
 import { Provider } from "react-redux";
-// import Meta from "../components/Meta";
+import Meta from "../components/Meta";
 import Layout from "../components/Layout";
 import CheckLogin from "../components/Login/checkLogin";
 function MyApp({ Component, pageProps }) {
