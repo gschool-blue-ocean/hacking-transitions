d8c8f2f44ee4ec9dd0f9ae5ee5218a708cfcd78f
Paul McDonald <pmcdonald1012@gmail.com>
2022-10-12 12:54:26 -0400

    added a few things to the migration file, and added crud on admin

--- components/AdminPage/EditStudentModal.js
+++ components/AdminPage/EditStudentModal.js
@@ -5,10 +5,21 @@ import { FiDelete } from 'react-icons/fi'
 import { BiArchiveOut} from 'react-icons/bi'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { motion } from 'framer-motion';
+import axios from 'axios';
 
 
 
 function EditStudentModal(props) {
+  const student_id = props.student_id;
+   const handleDelete = (event) => {
+    axios({
+      method:'delete',
+      url: `/api/users/${student_id}`,
+    }).then(res => console.log(res))
+   }
+   const handleArchive = (event) => {
+
+   }
     return (
       <Modal
         {...props}
@@ -23,10 +34,10 @@ function EditStudentModal(props) {
         </Modal.Header>
         <Modal.Body>
             <div> 
-                <Button variant='danger' style={{
+                <Button variant='danger' onClick={handleDelete} style={{
                     marginLeft: '1rem',
-                }}>DELETE  <FiDelete size={60} /></Button>
-                <Button variant='success' style={{
+                }}>Delete  <FiDelete size={60} /></Button>
+                <Button variant='success' onClick={handleArchive} style={{
                     marginLeft: '1rem',
                 }}>Archive  <AiOutlineEdit size={60} /></Button>
             </div>
@@ -34,19 +45,17 @@ function EditStudentModal(props) {
       </Modal>
     );
   }
-  function App() {
+  function App({student_id, currCohort}) {
     const [modalShow, setModalShow] = useState(false);
   
     return (
       <>
-       {/* DELETE BUTTON */}
-       
-        {/* ARCHIVE BUTTON */}
         <btn variant="primary" onClick={() => setModalShow(true)}>
          <AiOutlineEdit />
         </btn>
          
         <EditStudentModal
+          student_id={student_id}
           show={modalShow}
           onHide={() => setModalShow(false)}
         />
