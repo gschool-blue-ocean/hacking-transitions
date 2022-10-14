51955458cdf2432a23d95a737c8848b7134f32d3
Ilsa <hamptonilsa96@gmail.com>
2022-10-14 17:10:24 -0400

    some styling

--- components/Archive/index.js
+++ components/Archive/index.js
@@ -2,28 +2,61 @@ import { useState } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import style from "../../styles/Archive.module.css";
 
-//goal for this component
-//archive students button, puts checkboxes next to student names, can select students then push "OK"--> get message that student was archived
-//archive cohort button, puts checkboxes next to cohorts, can select multiple then push "OK"--> get message that cohort is archived
-//once state of "archived" should no longer display on other screens
-//should have a display archived button
-
-//now we only display the archived
-
-//make the rows today row: archived| false until archived then PATCH request and change archived to TRUE
-
 export default function ArchivePage() {
   //STATES NEEDED: global=> archived(student/cohort data), local=> checked(boolean), chooseCohorts(boolean), chooseStudents(boolean)
-  const mcsp = {
-    title: "MCSP-13",
-    dates: "10/08/2010-01/01/2011",
-    graduationDate: "21 OCT 22",
-    total: 30,
-    studentsClearing: 10,
-    studentsCleared: 10,
-    studentsNotClearing: 10,
-    students: "long list of students",
-  };
+  const [front, setFront] = useState(true);
+  const [displayCohorts, setDisplay] = useState([
+    {
+      title: "MCSP-13",
+      dates: "10/08/2010-01/01/2011",
+      graduationDate: "21 OCT 22",
+      total: 30,
+      studentsClearing: 10,
+      studentsCleared: 10,
+      studentsNotClearing: 10,
+      students: "long list of students",
+    },
+    {
+      title: "MCSP-13",
+      dates: "10/08/2010-01/01/2011",
+      graduationDate: "21 OCT 22",
+      total: 30,
+      studentsClearing: 10,
+      studentsCleared: 10,
+      studentsNotClearing: 10,
+      students: "long list of students",
+    },
+    {
+      title: "MCSP-13",
+      dates: "10/08/2010-01/01/2011",
+      graduationDate: "21 OCT 22",
+      total: 30,
+      studentsClearing: 10,
+      studentsCleared: 10,
+      studentsNotClearing: 10,
+      students: "long list of students",
+    },
+    {
+      title: "MCSP-13",
+      dates: "10/08/2010-01/01/2011",
+      graduationDate: "21 OCT 22",
+      total: 30,
+      studentsClearing: 10,
+      studentsCleared: 10,
+      studentsNotClearing: 10,
+      students: "long list of students",
+    },
+    {
+      title: "MCSP-13",
+      dates: "10/08/2010-01/01/2011",
+      graduationDate: "21 OCT 22",
+      total: 30,
+      studentsClearing: 10,
+      studentsCleared: 10,
+      studentsNotClearing: 10,
+      students: "long list of students",
+    },
+  ]);
 
   const student = {
     first: "bob",
@@ -47,17 +80,22 @@ export default function ArchivePage() {
           </button>
         </form>
         <div className={style.cardDeck}>
-          <div className={style.card}>
-            <h3>{mcsp.title}</h3>
-            <h4>{mcsp.total} students</h4>
-            <h4>{mcsp.dates}</h4>
-          </div>
-          <div className={style.card}>
-            <h3>
-              {student.first} {student.last}
-            </h3>
-            <h5>{student.cohort}</h5>
-          </div>
+          {displayCohorts.map((e) => {
+            return (
+              <>
+                <div className={style.card}>
+                  <h3>{e.title}</h3>
+                  <p>{e.total} students</p>
+                  <p>{e.dates}</p>
+                </div>
+
+                {/* <div className={style.card}>
+                  <h3>List of Students</h3>
+                  <h4>Students cleared: {e.studentsCleared}</h4>
+                </div> */}
+              </>
+            );
+          })}
         </div>
       </div>
 
@@ -69,9 +107,7 @@ export default function ArchivePage() {
             <HiOutlineSearch />
           </button>
         </form>
-        <div className={style.cardDeck}>
-          <div className={style.card}></div>
-        </div>
+        <div className={style.cardDeck}></div>
       </div>
     </div>
   );
