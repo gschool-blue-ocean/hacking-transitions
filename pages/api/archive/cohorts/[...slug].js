22f17a647cd9412b13df1480ae9c75a7d6b3e783
Ilsa <hamptonilsa96@gmail.com>
2022-10-18 10:44:11 -0400

    cohort search is no longer case sensitive

--- pages/api/archive/cohorts/[...slug].js
+++ pages/api/archive/cohorts/[...slug].js
@@ -8,8 +8,14 @@ export default async function handler(req, res) {
 
   if (checkApiMethod(req, "GET") && slug) {
     try {
+      //make not case sensitive
+      const name = slug[0].split("-");
+      const mcsp = name[0].toUpperCase();
+      const MCSP = mcsp + "-" + name[1];
+
+      //actual search
       let searchCohorts =
-        await sql`SELECT * FROM cohorts WHERE archived = true AND cohort_name = ${slug}`;
+        await sql`SELECT * FROM cohorts WHERE archived = true AND cohort_name = ${MCSP}`;
       res.json(searchCohorts);
     } catch (error) {
       console.log(error);
