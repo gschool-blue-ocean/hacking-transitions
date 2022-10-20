b12c4eb923bfb1609851a1a6be14b4d07c536287
Moses <mov7963@gmail.com>
2022-10-20 08:55:23 -0700

    moved sql insertion to existing files, to keep auto seed running

--- database/seed.sql
+++ database/seed.sql
@@ -8,4 +8,12 @@ insert into cohorts (cohort_name, start_date, end_date, active, archived, regist
 ,('MCSP-20', '12/6/2022', '4/7/2023', true, false, 'fromTheTopMakeItDrop')
 ,('MCSP-21', '1/10/2023', '5/12/2023', true, false, 'password');
 
+INSERT INTO cohorts (cohort_name, start_date, end_date, active, archived) values 
+
+('MCSP-02', '4/5/2020', '8/12/2020', false, true)
+,('MCSP-03', '8/16/2020', '12/16/2020', false, true)
+,('MCSP-04', '9/13/2020', '1/13/2020', false, true)
+,('MCSP-05', '10/11/2020', '2/10/2020', false, true)
+,('MCSP-06', '11/8/2020', '3/10/2020', false, true);
+
 \ir usersSeed.sql
