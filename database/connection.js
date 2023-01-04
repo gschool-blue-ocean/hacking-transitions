a129b5a94c7d60e540b630d0e564c30d182ff0b5
Lowjoejoe <joseph.t.low@gmail.com>
2023-01-04 10:24:17 -0800

    added Connection String

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,10 @@ const config = require('./dataConnectionConfig')[process.env.NODE_ENV||"dev"]
 const CONNECTION_STRING = config.connectionString
 process.env.CONNECTION_STRING = CONNECTION_STRING
 
+console.log("log Connection String:", CONNECTION_STRING)
+
 const sql = postgres(process.env.CONNECTION_STRING,{ idle_timeout: 1000 });
+
 export default sql;
 
 
