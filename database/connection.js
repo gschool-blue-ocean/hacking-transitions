6dc1a71d080dec422f8989b9ad786e6024831094
Lowjoejoe <joseph.t.low@gmail.com>
2023-01-04 13:00:12 -0800

    took away connectionString trying using DATABASE_URL through .env file

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,6 @@
 import postgres from "postgres";
-const config = require('./dataConnectionConfig')[process.env.NODE_ENV||"dev"]
-const CONNECTION_STRING = config.connectionString
-//process.env.CONNECTION_STRING = CONNECTION_STRING
 
-console.log("log Connection String:", CONNECTION_STRING)
-
-const sql = postgres(process.env.CONNECTION_STRING,{ idle_timeout: 1000 });
+const sql = postgres(process.env.DATABASE_URL,{ idle_timeout: 1000 });
 
 export default sql;
 
--- database/dataConnectionConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    production:{
-        connectionString: process.env.POSTGRES_CONNECTION_STRING +"?ssl=true"
-    },
-    dev: {
-
-        connectionString: 'postgresql://postgres:docker@localhost:5432/blueocean'
-    }
-}
